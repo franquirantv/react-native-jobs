0a0152274904082e2c378b921d90fa701fcd81fb
Francisco Quirant Vicente <fqv2@gcloud.ua.es>
2023-08-22 19:42:53 +0200

    Add: Search functionality + Jobs Details

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -5,42 +5,38 @@ import { checkImageURL } from '../../../../utils'
 
 const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
   return (
-    console.log(item.employer_logo),
-    console.log(item.employer_name),
-    console.log(item.job_title),
-    console.log(item.job_publisher),
-    console.log(item.job_country),
-      <TouchableOpacity
-        style={styles.container(selectedJob, item)}
-        onPress={() => handleCardPress(item)}
-      >
-        <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
-          <Image
-            source={{
-              uri: checkImageURL(item?.employer_logo)
-                ? item.employer_logo
-                : 'https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg'
-            }}
-            resizeMode='contain'
-            style={styles.logoImage}
-          />
-        </TouchableOpacity>
-        <Text style={styles.companyName} numberOfLines={1}>
-          {item.employer_name}
-        </Text>
 
-        <View style={styles.infoContainer}>
-          <Text style={styles.jobName(selectedJob, item)} numberOfLines={1}>
-            {item.job_title}
-          </Text>
-          <View style={styles.infoWrapper}>
-            <Text style={styles.publisher(selectedJob, item)}>
-              {item?.job_publisher} -
-            </Text>
-            <Text style={styles.location}> {item.job_country}</Text>
-          </View>
-        </View>
+    <TouchableOpacity
+      style={styles.container(selectedJob, item)}
+      onPress={() => handleCardPress(item)}
+    >
+      <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
+        <Image
+          source={{
+            uri: checkImageURL(item?.employer_logo)
+              ? item.employer_logo
+              : 'https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg'
+          }}
+          resizeMode='contain'
+          style={styles.logoImage}
+        />
       </TouchableOpacity>
+      <Text style={styles.companyName} numberOfLines={1}>
+        {item.employer_name}
+      </Text>
+
+      <View style={styles.infoContainer}>
+        <Text style={styles.jobName(selectedJob, item)} numberOfLines={1}>
+          {item.job_title}
+        </Text>
+        <View style={styles.infoWrapper}>
+          <Text style={styles.publisher(selectedJob, item)}>
+            {item?.job_publisher} -
+          </Text>
+          <Text style={styles.location}> {item.job_country}</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
   )
 }
 
